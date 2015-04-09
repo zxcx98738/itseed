c749c7f5586a84c8ecf6379e426daa89b461b27b
Casey Chang <b00705008@ntu.edu.tw>
2015-04-09 17:01:32 +0800

    加入同步機制

--- api/controllers/VideoController.js
+++ api/controllers/VideoController.js
@@ -30,98 +30,110 @@ module.exports = {
     list: function(req, res){
         /*if(req.session.type == "admin"){*/
             var status = req.param('status');
-            var total, draftNum, publishNum;
 
-            Video.count()
-            .exec(function(err, count){
-                total = count;
-            });
-            Video.count({
-                status: "D"
-            }).exec(function(err, count){
-                draftNum = count;
-            });
-            Video.count({
-                status: "P"
-            }).exec(function(err, count){
-                publishNum = count;
-            });
-
-            if(status == "all"){
-                Video.find()
-                .sort({ order: "asc" })
-                .exec(function(err, videos){
-                    if(err)
-                        res.end(JSON.stringify(err));
-                    else{
-                        if(videos.length == 0){
-                            res.end("還沒有影片啦");
-                        }
-                        else{
-                            return res.view("backend/pages/cms", {
-                                articles: videos,
-                                editing: "video",
-                                status: "all",
-                                total: total,
-                                draftNum: draftNum,
-                                publishNum: publishNum,
-                                scheduleNum: 0
-                            });
-                        }
+            async.series({
+                total: function(callback){
+                    Video.count()
+                    .exec(function(err, count){
+                        callback(null, count);
+                    });
+                },
+                draftNum: function(callback){
+                    Video.count({
+                        status: "D"
+                    }).exec(function(err, count){
+                        callback(null, count);
+                    });
+                },
+                publishNum: function(callback){
+                    Video.count({
+                        status: "P"
+                    }).exec(function(err, count){
+                        callback(null, count);
+                    });
+                },
+            },
+            function(err, results){
+                if(err){
+                    res.end(JSON.stringify(err));
+                }
+                else {
+                    if(status == "all"){
+                        Video.find()
+                        .sort({ order: "asc" })
+                        .exec(function(err, videos){
+                            if(err)
+                                res.end(JSON.stringify(err));
+                            else{
+                                if(videos.length == 0){
+                                    res.end("還沒有影片啦");
+                                }
+                                else{
+                                    return res.view("backend/pages/cms", {
+                                        articles: videos,
+                                        editing: "video",
+                                        status: "all",
+                                        total: results.total,
+                                        draftNum: results.draftNum,
+                                        publishNum: results.publishNum,
+                                        scheduleNum: 0
+                                    });
+                                }
+                            }
+                        });
                     }
-                });
-            }
-            else if(status == "draft"){
-                Video.find()
-                .where({ status: "D" })
-                .sort({ order: "asc" })
-                .exec(function(err, videos){
-                    if(err)
-                        res.end(JSON.stringify(err));
-                    else{
-                        if(videos.length == 0){
-                            res.end("還沒有影片啦");
-                        }
-                        else{
-                            return res.view("backend/pages/cms", {
-                                articles: videos,
-                                editing: "video",
-                                status: "draft",
-                                total: total,
-                                draftNum: draftNum,
-                                publishNum: publishNum,
-                                scheduleNum: 0
-                            });
-                        }
+                    else if(status == "draft"){
+                        Video.find()
+                        .where({ status: "D" })
+                        .sort({ order: "asc" })
+                        .exec(function(err, videos){
+                            if(err)
+                                res.end(JSON.stringify(err));
+                            else{
+                                if(videos.length == 0){
+                                    res.end("還沒有影片啦");
+                                }
+                                else{
+                                    return res.view("backend/pages/cms", {
+                                        articles: videos,
+                                        editing: "video",
+                                        status: "draft",
+                                        total: results.total,
+                                        draftNum: results.draftNum,
+                                        publishNum: results.publishNum,
+                                        scheduleNum: 0
+                                    });
+                                }
+                            }
+                        });
                     }
-                });
-            }
-            else {
-                Video.find()
-                .where({ status: "P" })
-                .sort({ order: "asc" })
-                .exec(function(err, videos){
-                    if(err)
-                        res.end(JSON.stringify(err));
-                    else{
-                        if(videos.length == 0){
-                            res.end("還沒有影片啦");
-                        }
-                        else{
-                            return res.view("backend/pages/cms", {
-                                articles: videos,
-                                editing: "video",
-                                status: "publish",
-                                total: total,
-                                draftNum: draftNum,
-                                publishNum: publishNum,
-                                scheduleNum: 0
-                            });
-                        }
+                    else {
+                        Video.find()
+                        .where({ status: "P" })
+                        .sort({ order: "asc" })
+                        .exec(function(err, videos){
+                            if(err)
+                                res.end(JSON.stringify(err));
+                            else{
+                                if(videos.length == 0){
+                                    res.end("還沒有影片啦");
+                                }
+                                else{
+                                    return res.view("backend/pages/cms", {
+                                        articles: videos,
+                                        editing: "video",
+                                        status: "publish",
+                                        total: results.total,
+                                        draftNum: results.draftNum,
+                                        publishNum: results.publishNum,
+                                        scheduleNum: 0
+                                    });
+                                }
+                            }
+                        });
                     }
-                });
-            }
-            
+                }
+            });
         /*}
         else{
             return res.view("redirect", {
