e43d3dcd7ab4a880de11b8ff62bcb921a746b1f0
leo <aleoliu566@gmail.com>
2017-07-07 17:10:40 +0800

    reginfo調整

--- tasks/config/watch.js
+++ tasks/config/watch.js
@@ -27,7 +27,13 @@ module.exports = function(grunt) {
 
 			// When assets are changed:
 			tasks: ['syncAssets' , 'linkAssets']
-		}
+		},
+		views: {
+            files: ['views/**/*']
+    },
+		options: {
+      livereload: true,
+    },
 	});
 
 	grunt.loadNpmTasks('grunt-contrib-watch');
