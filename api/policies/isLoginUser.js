cb8416bb3b71112bcf9c6fa69431144ff7430461
q40603 <q40603@gmail.com>
2019-04-17 15:03:20 +0800

    remove log

--- api/policies/isLoginUser.js
+++ api/policies/isLoginUser.js
@@ -11,7 +11,6 @@ module.exports = function (req, res, next) {
 
     // User is allowed, proceed to the next policy,
     // or if this is the last policy, the controller
-    console.log(typeof req.session.authorized);
     //console.log(req.session.authorized.user);
     //console.log(req.session)
     if (typeof req.session.authorized !== "undefined" && req.session.authorized.user == true){
