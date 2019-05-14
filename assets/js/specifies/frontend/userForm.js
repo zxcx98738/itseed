a5453fe21d75c53c520284e59306e849c69e4d3c
q40603 <q40603@gmail.com>
2019-05-14 23:10:07 +0800

    rm old sub step bar

---
+++ assets/js/specifies/frontend/userForm.js
@@ -0,0 +1,22 @@
+$(function(){
+  /*表單驗證*/
+
+	$("form").validate({
+		submitHandler: function(form) {
+      form.submit();
+    },
+    errorPlacement: function(error, element) {
+      element.closest('div').append(error);
+      element.closest('div').addClass('error');
+    },
+    success: function(element) {
+      element.closest('div').removeClass('error');
+    },
+  });
+
+});
+$( document ).ready(function() {
+  $("#disc_progress").addClass('active');
+  $("#profile_progress").addClass('active');
+  $("#form_progress").addClass('active');
+});
