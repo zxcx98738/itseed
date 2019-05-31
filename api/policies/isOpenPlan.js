0ec6097ceea37d6712648908b1c3c3e1ab58c22c
q40603 <q40603@gmail.com>
2019-06-01 02:44:37 +0800

    system open

--- api/policies/isOpenPlan.js
+++ api/policies/isOpenPlan.js
@@ -12,7 +12,7 @@ module.exports = function (req, res, next) {
     // User is allowed, proceed to the next policy,
     // or if this is the last policy, the controller
     let nowTime = new Date();
-    let openTime = new Date('2019/5/31');
+    let openTime = new Date('2019/5/30');
     if (nowTime > openTime){
         return next();
     }else{
